7db065aeb5f0c216b99a5f6a99be00c39cd87dce
myeongsoyi <myeongsoyi@gmail.com>
2025-01-06 00:19:07 +0900

    feat: NAT gateway 생성

--- main.tf
+++ main.tf
@@ -28,3 +28,27 @@ resource "aws_subnet" "public" {
     Name = each.key
   }
 }
+
+resource "aws_subnet" "private" {
+  for_each = var.private_subnets
+
+  vpc_id =  aws_vpc.main.id
+  cidr_block = each.value.cidr_block
+  availability_zone = each.value.availability_zone
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = each.key
+  }
+}
+
+resource "aws_eip" "nat_ips" {
+  count = var.private_subnets != {} ? length(var.private_subnets) : 0
+}
+
+resource "aws_nat_gateway" "gateways" {
+  count = var.private_subnets != {} ? length(var.private_subnets) : 0
+
+  allocation_id = aws_eip.nat_ips[count.index].id
+  subnet_id = tolist(values(aws_subnet.public))[count.index].id
+}
--- variables.tf
+++ variables.tf
@@ -17,3 +17,10 @@ variable "public_subnets" {
     cidr_block = string
   }))
 }
+
+variable "private_subnets" {
+  type = map(object({
+    availability_zone = string
+    cidr_block = string
+  }))
+}
